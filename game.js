bff2d3fdab25fb71eecbe9eec93382846c732699
Jivenom <davidwolverton3@gmail.com>
2013-06-01 22:13:39 -0400

    Update game.js

--- game.js
+++ game.js
@@ -16,11 +16,11 @@ function bootstrap(Bouvardiaversion){
 	home_0 = [];
 	
 	home_0["name"] = "Home";
-	home_0["description"] = "This is you House.";
+	home_0["description"] = "This is your House.";
 	home_0["connectsWest"] = undefined;
-	home_0["connectsEast"] = 1;
+	home_0["connectsEast"] = undefined;
 	home_0["connectsNorth"] = undefined;
-	home_0["connectsSouth"] = undefined;
+	home_0["connectsSouth"] = 1;
 	home_0["item"] = 0;
 	home_0["requiredItem"] = undefined;
 	
@@ -28,16 +28,16 @@ function bootstrap(Bouvardiaversion){
 	
 	room_1 = []
 	
-	room_1["name"] = "Room One";
-	room_1["description"] = "This. Is. Room. One.";
-	room_1["connectsWest"] = 0;
-	room_1["connectsEast"] = undefined;
-	room_1["connectsNorth"] = undefined;
-	room_1["connectsSouth"] = undefined;
-	room_1["item"] = undefined;
-	room_1["requiredItem"] = 0;
+	driveway_1["name"] = "Driveway";
+	driveway_1["description"] = "Thisis your Driveway. It is small and can hold one car.";
+	driveway_1["connectsWest"] = 0;
+	driveway_1["connectsEast"] = undefined;
+	driveway_1["connectsNorth"] = undefined;
+	driveway_1["connectsSouth"] = undefined;
+	driveway_1["item"] = undefined;
+	driveway_1["requiredItem"] = 0;
 	
-	addRoom(room_1);
+	addRoom(driveway_1);
 	
 	item_0 = []
 	item_0["name"] = "House Key";
